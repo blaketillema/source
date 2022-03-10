945a84ee1ae8ac378583bf1a6f24133729524fdf
unknown <blaketillema@gmail.com>
2022-03-10 23:07:33 +1000

    ass

--- fart.js
+++ fart.js
@@ -1,14 +1,13 @@
-let fart = new Audio('fart.mp3');
-
 function f(){
+
+    ass = document.getElementsByClassName('assClass')[0];
+    ass.style.display = "flex";
+    let fart = new Audio('fart.mp3');
     fart.play();
 
     function delay(time) {
         return new Promise(resolve => setTimeout(resolve, time));
     }
-
-    ass = document.getElementsByClassName('assClass')[0];
-    ass.style.display = "flex";
 
     delay(1000).then(() => {
         ass.style.display = "none";
